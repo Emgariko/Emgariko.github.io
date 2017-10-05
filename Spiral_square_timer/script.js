b6300ccf629d2f9ee3e757fee86c8f83c835f6c1
Emgariko <emil2001garipov@gmail.com>
2017-10-05 10:04:12 +0300

    Add files via upload

---
+++ Spiral_square_timer/script.js
@@ -0,0 +1,209 @@
+var example = document.getElementById("example"),
+    ctx     = example.getContext('2d');
+    var x = window.innerWidth;
+    var y = window.innerHeight - 30;
+    example.height = window.innerHeight - 30;
+    example.width  = window.innerWidth;
+
+function draw(){   
+    ctx.fillStyle = colb;
+    ctx.fillRect(0, 0, x, y);       
+}
+
+function getRndColor() {
+    var r = 255*Math.random()|0,
+        g = 255*Math.random()|0,
+        b = 255*Math.random()|0;
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}   
+
+
+function pt(x, y){
+    var p = {x, y};
+    p.x = x;
+    p.y = y;
+    return p;       
+}
+
+function drawline(a, b, color, w){
+    var x1 = a.x;
+    var y1 = a.y;
+    var x2 = b.x;
+    var y2 = b.y;
+    y1 = y - y1;
+    y2 = y - y2;
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = w;
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2); 
+    ctx.stroke();
+    //alert(x1 + ' ' + y1 + ' ' + x2 + ' ' + y2);
+}
+
+function drawrect(a1, a2, a3, a4, col, w){
+    drawline(a1, a2, col, w);
+    drawline(a2, a3, col, w);
+    drawline(a3, a4, col, w);
+    drawline(a4, a1, col, w);
+}
+            
+function setlen(a, len){
+    var x1 = a.x;
+    var y1 = a.y;
+    var l = Math.sqrt(x1 * x1 + y1 * y1);
+    x1 = x1 / l;
+    y1 = y1 / l;
+    x1 = x1 * len;
+    y1 = y1 * len;
+    var res = {};
+    res.x = x1;
+    res.y = y1;
+    //alert(x1 + ' ' + y1);
+    return res;
+}
+
+
+function rotate(a, an){
+    //alert(a.x + ' ' + a.y);
+    var x1 = a.x;
+    var y1 = a.y;                
+    var s = Math.sin(an);
+    var c = Math.cos(an);
+    /*if (alpha > 0.0){
+        s = 0.707106781;
+        c = 0.707106781;
+    }
+    else{
+        s = -0.707106781;
+        c = -0.707106781;
+    } */    
+    //alert(s + ' ' + ' ' + c);
+    var res = {};
+    res.x = x1 * s - y1 * c;
+    res.y = x1 * c + y1 * s;
+    //alert(res.x + ' ' + res.y + ' alpha = ' + alpha);
+    return res;
+}
+
+function rotate90(a){
+    var x1 = a.x;
+    var y1 = a.y;
+    var res = pt(-y1, x1);
+    return res;
+}                 
+
+function dist(a, b){
+    var x1 = a.x;
+    var x2 = b.x;
+    var y1 = a.y;
+    var y2 = b.y;
+    return Math.sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}
+ 
+function drawr(a, b, c, d, col, w, alpha, i){
+    var dd = dist(a, b);
+    if (dd < 1)
+        return;
+    else{
+    drawrect(a, b, c, d, col[i % col.length], w);
+    var ab = pt(b.x - a.x, b.y - a.y);
+    var ad = pt(d.x - a.x, d.y - a.y);
+    var l1 = dd / (Math.sin(alpha) + Math.cos(alpha));
+    var l2 = Math.cos(alpha) * l1;
+    var l3 = Math.sin(alpha) * l1;
+    ab = setlen(ab, l2);
+    ad = setlen(ad, l3);     
+    var a1, b1, c1, d1;
+    a1 = pt(a.x + ab.x, a.y + ab.y);
+    d1 = pt(a.x + ad.x, a.y + ad.y);
+    c1 = pt(c.x - ab.x, c.y - ab.y);
+    b1 = pt(c.x - ad.x, c.y - ad.y); 
+    //alert(a1.x + ' ' + a1.y + "\n" + a2.x + ' ' + a2.y + "\n" + a3.x + ' ' + a3.y + "\n" + a4.x + ' ' + a4.y);
+    drawr(a1, b1, c1, d1, col, w, alpha, i + 1);     
+    }
+} 
+  
+draw();            
+
+var col = [];
+var i;
+
+for (i = 0; i < 1000; i++){
+    col.push(getRndColor());   
+}
+
+alpha = Math.PI / 4;
+var delta = 0.008;
+var au = 0;
+var colb = "rgb(0, 0, 0)";
+
+function plus(){    
+    if (au != 1)
+        delta *= 1.5;
+    delta = min(delta, 0.5);
+}
+
+function minus(){
+    if (au != 1)
+        delta /= 1.5;
+    delta = max(delta, 0.000000000000001);
+}
+
+function auto(){
+    au = 1 - au;
+}
+
+function rnd(){
+    for (i = 0; i < col.length; i++){
+        col[i] = getRndColor();
+    }
+}
+
+function setcolor(){
+    var x = document.getElementById("myColor").value;
+    //alert(x);
+    for (i = 0; i < col.length; i++){
+        col[i] = x;
+    }
+}
+
+function setcolorbg(){
+    var x = document.getElementById("mybgColor").value;
+    colb = x;
+}
+
+var timerid = setInterval(function(){
+    draw();                
+    alpha += delta;
+    if (alpha + delta - Math.PI / 2 > 0.0000000001)  {
+        delta *= -1;
+        alpha = Math.PI / 2 - 0.0000000000001;        
+    }
+    if (alpha + delta < 0.000000001){
+        delta *= -1;
+        alpha = 0.0000000001;    
+    }
+    var c = pt(0, 0);
+    var w = 1.75;
+    var a1, a2, a3, a4;
+    if (x > y){
+        c = pt(x / 2, y / 2);
+        a1 = pt(c.x - y / 2, w);
+        a2 = pt(c.x + y / 2, w);
+        a3 = pt(c.x + y / 2, y - w);
+        a4 = pt(c.x - y / 2, y - w);    
+    }
+    else{
+        c = pt(x / 2, y / 2);
+        a1 = pt(x - 2, c.y - x / 2);
+        a2 = pt(x - 2, c.y + x / 2);
+        a3 = pt(0, c.y + x / 2);
+        a4 = pt(0, c.y - x / 2);
+    }                  
+    //drawrect(a1, a2, a3, a4, "rgb(255, 0, 0)", 2);  
+    //alert(a1.x + ' ' + a1.y + "\n" + a2.x + ' ' + a2.y + "\n" + a3.x + ' ' + a3.y + "\n" + a4.x + ' ' + a4.y); 
+    drawr(a1, a2, a3, a4, col, w, alpha, 0);
+}, 200);
+
+drawfr();
---
+++ Tree/script.js
@@ -0,0 +1,231 @@
+var example = document.getElementById("example"),
+    ctx     = example.getContext('2d');
+    var x = window.innerWidth;
+    var y = window.innerHeight;
+    example.height = window.innerHeight - 30;
+    example.width  = window.innerWidth;
+
+function draw(){   
+    ctx.fillStyle = 'rgb(0, 0, 0)';
+    ctx.fillRect(0, 0, x, y);       
+}
+
+function getRndColor() {
+    var r = 255*Math.random()|0,
+        g = 255*Math.random()|0,
+        b = 255*Math.random()|0;
+    return 'rgb(' + r + ',' + g + ',' + b + ')';
+}   
+
+
+function pt(x, y){
+    var p = {x, y};
+    p.x = x;
+    p.y = y;
+    return p;       
+}
+
+function drawline(a, b, color, w){
+    var x1 = a.x;
+    var y1 = a.y;
+    var x2 = b.x;
+    var y2 = b.y;
+    y1 = y - y1;
+    y2 = y - y2;
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.lineWidth = w;
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2); 
+    ctx.stroke();
+    //alert(x1 + ' ' + y1 + ' ' + x2 + ' ' + y2);
+}
+
+function drawrect(a, b, c, d, color, w, i){
+   /* drawline(a, b, color, w);
+    drawline(b, c, color, w);
+    drawline(c, d, color, w);
+    drawline(d, a, color, w); */
+    var x1 = a.x;
+    var y1 = a.y;
+    var x2 = b.x;
+    var y2 = b.y;
+    var x3 = c.x;
+    var y3 = y - c.y;
+    var x4 = d.x;
+    var y4 = y - d.y;
+    y1 = y - y1;
+    y2 = y - y2;       
+    if (i > 0){ 
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = w;
+        ctx.moveTo(x2, y2);
+        ctx.lineTo(x3, y3);
+        //ctx.moveTo(x3, y3);
+        ctx.lineTo(x4, y4);
+        //ctx.moveTo(x4, y4);
+        ctx.lineTo(x1, y1);
+        ctx.lineTo(x2, y2);
+        if (z == 1){
+            ctx.fillStyle = color;
+            ctx.fill();
+        }
+        ctx.stroke();     
+    }
+    else{
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = w;
+        ctx.moveTo(x1, y1);    
+        ctx.lineTo(x2, y2);
+        //ctx.moveTo(x2, y2);
+        ctx.lineTo(x3, y3);
+        //ctx.moveTo(x3, y3);
+        ctx.lineTo(x4, y4);
+        //ctx.moveTo(x4, y4);
+        ctx.lineTo(x1, y1);
+        if (z == 1){
+            ctx.fillStyle = color;
+            ctx.fill();
+        }
+        ctx.stroke();
+    }                     
+}
+
+function drawtr(a, b, c, color, w){
+    var x1 = a.x;
+    var y1 = y - a.y;
+    var x2 = b.x;
+    var y2 = y - b.y;
+    var x3 = c.x;
+    var y3 = y - c.y;
+    ctx.beginPath();
+    ctx.strokeStyle = color;
+    ctx.moveTo(x1, y1);
+    ctx.lineTo(x2, y2);
+    ctx.lineTo(x3, y3);
+    if (z == 1){
+        ctx.fillStyle = colb;
+        ctx.fill();
+    }
+    ctx.stroke();
+}
+ 
+function setlen(a, len){
+    var x1 = a.x;
+    var y1 = a.y;
+    var l = Math.sqrt(x1 * x1 + y1 * y1);
+    x1 = x1 / l;
+    y1 = y1 / l;
+    x1 = x1 * len;
+    y1 = y1 * len;
+    var res = {};
+    res.x = x1;
+    res.y = y1;
+    //alert(x1 + ' ' + y1);
+    return res;
+}
+
+
+function rotate(a, an){
+    //alert(a.x + ' ' + a.y);
+    var x1 = a.x;
+    var y1 = a.y;                
+    var s = Math.sin(an);
+    var c = Math.cos(an);
+    /*if (alpha > 0.0){
+        s = 0.707106781;
+        c = 0.707106781;
+    }
+    else{
+        s = -0.707106781;
+        c = -0.707106781;
+    } */    
+    //alert(s + ' ' + ' ' + c);
+    var res = {};
+    res.x = x1 * s - y1 * c;
+    res.y = x1 * c + y1 * s;
+    //alert(res.x + ' ' + res.y + ' alpha = ' + alpha);
+    return res;
+}
+
+function rotate90(a){
+    var x1 = a.x;
+    var y1 = a.y;
+    var res = pt(-y1, x1);
+    return res;
+}                 
+
+function tree(a, b, an, len, i){
+    if (len < 3){
+        //alert(i);
+        return;  
+    }
+    else{
+        //alert('x1 = ' + a.x + ' ' + 'y1 = ' + a.y + '\n' + 'x2 = ' + b.x + ' ' + 'y2 = ' + b.y);
+        //alert(a.x + ' ' + a.y + ' ' + b.x + ' ' + b.y);
+        var x1 = a.x;
+        var y1 = a.y;
+        var x2 = b.x;
+        var y2 = b.y;                          
+        var ad = rotate90(pt(x2 - x1, y2 - y1));  
+        var d = pt(x1 + ad.x, y1 + ad.y); 
+        var c = pt(x2 + ad.x, y2 + ad.y);
+        //alert('x1 = ' + x1 + ' y1 = ' + y1 + ' x2 = ' + x2 + ' y2 = ' + y2 + '\n x3 = ' + c.x + ' y3 = ' + c.y + ' x4 = ' + d.x + ' y4 = ' + d.y);
+        drawrect(a, b, c, d, cola, ww, i);
+        var l1 = len * Math.sin(an);
+        var l2 = len * Math.cos(an);
+        var dc = pt(c.x - d.x, c.y - d.y);
+        dc = rotate(dc, an);    
+        dc = setlen(dc, l1);
+        var e = pt(d.x + dc.x, d.y + dc.y);
+        //alert('x5 = ' + e.x + ' y5 = ' + e.y);
+        drawtr(d, c, e, cola, ww);
+        tree(d, e, an, l1, i + 1);
+        tree(e, c, an, l2, i + 1); 
+    }
+}           
+
+var alpha;
+
+
+draw();                     
+var col = 'rgb(255, 0, 255)';
+var z = 0;
+var ww = 1;
+alpha = Math.PI / 4;
+var cola = 'rgb(0, 255, 0)';
+var colb = 'rgb(0, 255, 255)';
+function drawtree(){
+    alpha = document.getElementById("angle").value;
+    //x = window.innerWidth;
+    //y = (1 + Math.abs(alpha - 45)) / 20 *  window.innerHeight - 30;
+    //example.height =(1 + Math.abs(alpha - 45)) *  window.innerHeight - 30;
+    //example.width  = window.innerWidth;
+    alpha = alpha * Math.PI / 180;
+    //alert(alpha);
+    draw();
+    var k = (Math.PI) / alpha;
+    var x1 = x / 2;
+    var y1 = y / 6;        
+    //var len = (x / Math.abs(4 - k)) / 4;
+    var len = 120; 
+    var ll1 = x / 2;
+    var a = {};
+    var ll1 = (alpha * (x / 2 - len / 2)) / (Math.PI / 4);
+    var ll2 = ll1 + len;
+    if (ll1 > ll2){
+        var kek = ll2;
+        ll2 = ll1;
+        ll1 = kek;
+    }                                     
+    a.x = ll1;
+    a.y = y1;
+    var b = {};
+    b.x = ll2; 
+    b.y = y1;           
+    tree(a, b, alpha, len, 0);
+}
+                    
+drawtree();
